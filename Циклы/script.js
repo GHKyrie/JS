b99d05278c999efd956fc8080b3846f455c2f193
Denis <nemasterya@gmail.com>
2019-06-09 00:28:31 +0500

    fe

--- Циклы/script.js
+++ Циклы/script.js
@@ -1,5 +1,3 @@
-let num = 50;
-
 if (num < 49) {
     console.log("False")
 } else if (num > 100) {
@@ -27,3 +25,40 @@ switch (num) {
         console.log("Error");
         break;
 }
+
+// let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
+
+let i = 0;
+while (i < 8) {
+    i++;
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
+
+i = 0;
+do {
+    i++;
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+} while (i < 8);
